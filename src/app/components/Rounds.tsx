134a9b2360f17e38a74f573a4f308bc634fce3a6
ssebestyen <sarah@sebestyen.com>
2025-03-23 22:33:21 -0500

    clean up errors

--- src/app/components/Rounds.tsx
+++ src/app/components/Rounds.tsx
@@ -1,11 +1,4 @@
 import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionActions from '@mui/material/AccordionActions';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
 import Round from './Round';
 
 export default function AccordionUsage() {
